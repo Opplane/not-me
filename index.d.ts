a93e573a175db4814072e132f9e3591018d631e0
Bartolomeu <bart@opplane.com>
2021-02-19 12:39:41 +0000

    Add references to types for autocompletion

---
+++ index.d.ts
@@ -0,0 +1,19 @@
+/// <reference path="./lib/error-messages/default-messages/default-error-messages-manager.d.ts" />
+
+/// <reference path="./lib/error-messages/error-messages-tree.d.ts" />
+
+/// <reference path="./lib/resolvers/formik/formik-resolver.d.ts" />
+
+/// <reference path="./lib/resolvers/nest/validation-schema.decorator.d.ts" />
+/// <reference path="./lib/resolvers/nest/validation.pipe.d.ts" />
+
+/// <reference path="./lib/schemas/array/array-schema.d.ts" />
+/// <reference path="./lib/schemas/base/base-schema.d.ts" />
+/// <reference path="./lib/schemas/boolean/boolean-schema.d.ts" />
+/// <reference path="./lib/schemas/date/date-schema.d.ts" />
+/// <reference path="./lib/schemas/equals/equals-schema.d.ts" />
+/// <reference path="./lib/schemas/number/number-schema.d.ts" />
+/// <reference path="./lib/schemas/object/object-schema.d.ts" />
+/// <reference path="./lib/schemas/object/object-of-schema.d.ts" />
+/// <reference path="./lib/schemas/string/string-schema.d.ts" />
+/// <reference path="./lib/schemas/schema.d.ts" />
